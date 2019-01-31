085212973da003195c0266d1faee416211459835
nachiket <nachiketsdabhade@gmail.com>
2019-01-31 12:36:02 +0530

    integrated sockets

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import socketIOClient from "socket.io-client";
 import Todos from './Todos';
 import AddTodo from './AddTodo';
 
@@ -9,59 +10,49 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    fetch('http://localhost:5000/api/todos')
-      .then(res => res.json())
-      .then(res => this.setState({
-        todos: res.todoList,
-      }))
-      .catch(console.log)
-  }
-
-  deleteTodo = id => {
-    fetch("http://localhost:5000/api/delete", {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8"
-      },
-      body: JSON.stringify({ id })
-    })
-      .then(res => res.json())
-      .then(console.log)
-      .then(() => {
-        const todos = this.state.todos.filter(todo => {
-          return todo.id !== id
-        });
-        this.setState({
-          todos
-        });
-      })
-      .catch(console.log)
+    // For development on parallel servers
+    this.socket = socketIOClient("http://localhost:5000");
+    // For production after running npm run build
+    // this.socket = socketIOClient("/");
+    this.socket.on('connect', () => console.log('Connected to server'));
+    // Recieving all todos from the server and adding them to the state
+    this.socket.on('getAllTodos', todos => this.setState({
+      todos
+    }));
+    // Adding a new todo which has been sent by the server to the state
+    this.socket.on('addTodo', newTodo => {
+      let todos = [...this.state.todos, newTodo];
+      this.setState({
+        todos
+      });
+    });
+    // Delete a todo from state by filtering using the todo's id
+    this.socket.on('deleteTodo', id => {
+      const todos = this.state.todos.filter(todo => {
+        return todo.id !== id
+      });
+      this.setState({
+        todos
+      });
+    });
+    this.socket.on('disconnect', () => console.log('disconnect'));
   }
 
   addTodo = todo => {
     // To get a random id from 1 to 100
-    todo.id = Math.floor((Math.random() * 100) + 1)
-    fetch("http://localhost:5000/api/add", {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json;charset=UTF-8"
-      },
-      body: JSON.stringify(todo)
-    })
-      .then(res => res.json())
-      .then(console.log)
-      .then(() => {
-        let todos = [...this.state.todos, todo];
-        this.setState({
-          todos
-        });
-      })
-      .catch(console.log)
+    todo.id = Math.floor((Math.random() * 100) + 1);
+    // Emiting event with new todo to let server know we want to add it
+    this.socket.emit('newTodo', todo)
+  }
+
+  deleteTodo = id => {
+    // Emiting event with todo id which has to deleted by server
+    this.socket.emit('removeTodo', { id })
   }
 
   render() {
     return (
-      <div className="App container">
+      <div className="App container" >
         <div className="container">
           <h1 className="center grey-text text-darken-4">Todo's</h1>
           <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
--- server/app.py
+++ server/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
+from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
-app = Flask(__name__)
+# after npm run build paste the build folder inside server folder
+app = Flask(__name__, static_folder='./build/static',
+            template_folder='./build')
+app.config['SECRET_KEY'] = 'mysecretkeylmao'
 # For development
 cors = CORS(app)
+socketio = SocketIO(app)
 
+# dummy data that can be made dynamic by connection to any database of your choice
+# like MongoDb, MySQL, PostgreSQL etc
 todos = {
 	'todoList': [
 		{ 'id': 1, 'content': 'Buy some milk' },
@@ -12,27 +19,34 @@ todos = {
 	]
 }
 
-# Route to get all the todos
-@app.route('/api/todos', methods=["GET"])
-def index():
-  return jsonify(todos)
+# routes to render frontend after creating build version
+# @app.route('/', defaults={'path': ''}, methods=['GET'])
+# @app.route('/<path:path>', methods=['GET'])
+# def index(path):
+#     return render_template('index.html')
 
-# Route to add a new todo
-@app.route('/api/add', methods=["POST"])
-def addTodo():
-	id = request.get_json().get('id')
-	content = request.get_json().get('content')
-	newTodo = { 'id': id, 'content': content }
+# to notify and for sending all todos to new client as soon as they are connected
+@socketio.on('connect')
+def onConnection():
+	print('Connected')
+	emit('getAllTodos', todos['todoList'], broadcast=False)
+
+# send new todo data to all clients after adding it to local todoList
+@socketio.on('newTodo')
+def newTodo(newTodo):
 	todos['todoList'].append(newTodo)
-	return jsonify({'success': True })
+	emit('addTodo', newTodo, broadcast=True)
 
-# Route to delete a todo
-@app.route('/api/delete', methods=["POST"])
-def deleteTodo():
-	id = request.get_json().get('id')
-	todos['todoList'] = [i for i in todos['todoList'] if (i['id'] != id)]
-	return jsonify({ 'success': True })
+# sent todo id to be deleted after deleting from local todoList
+@socketio.on('removeTodo')
+def removeTodo(id):
+	todos['todoList'] = [i for i in todos['todoList'] if (i['id'] != id['id'])]
+	emit('deleteTodo', id['id'], broadcast=True)
+
+# to notify on disconnect
+@socketio.on('disconnect')
+def onDisconnect():
+	print('Disconnected')
 
 if __name__ == "__main__":
-	app.debug = True
-	app.run(host='localhost', port=5000)
+	socketio.run(app, host='localhost', port=5000)
